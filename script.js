efb5956fee869e007e8c3891ca78eca781be2251
Calif432 <caleblangat2004@gmail.com>
2024-11-20 11:32:22 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+/* var othernav= document.getElementById("othernav");
+function showMenu(){
+    console.log("hii")
+    //othernav.style.marginRight="0";
+    othernav.style.display="block"
+}
+function hideMenu(){
+    othernav.style.display="none"
+   // othernav.style.marginRight="-400px";
+} */
